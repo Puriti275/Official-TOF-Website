a06eff9c08d081f694612ad9b9f61daa6072eaa8
Puriti275 <puriti275@gmail.com>
2025-12-24 22:39:38 -0600

    Update app.js

--- Code/server/app.js
+++ Code/server/app.js
@@ -60,7 +60,7 @@ const __dirname = path.dirname(__filename)
 app.use(express.static(path.join(__dirname, '../client')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
 app.use(express.json())
